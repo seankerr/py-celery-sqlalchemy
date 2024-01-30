7ee144c754846fe22a371187116e07d5e27bc7d5
Sean Kerr <sean@code-box.org>
2024-01-30 09:33:24 -0800

    Remove root initialize()

--- celery_sqlalchemy/__init__.py
+++ celery_sqlalchemy/__init__.py
@@ -5,38 +5,3 @@
 """
 SQLAlchemy model serialization for celery.
 """
-
-# celery-sqlalchemy imports
-from . import errors
-
-# system imports
-from importlib import import_module
-
-from typing import Any
-
-# dependency imports
-from celery import Celery
-
-
-def initialize(
-    celery: Celery,
-    interface: str = "json",
-    **interface_args: Any,
-) -> None:
-    """
-    Initialize the serializer with models.
-
-    Parameters:
-        celery (Celery): Celery instance.
-        interface (str): Serialization interface.
-        interface_args (dict): Initialization arguments to pass to the interface.
-    """
-    try:
-        interface_module = import_module(f".{interface}", package="celery_sqlalchemy")
-
-    except ModuleNotFoundError:
-        raise errors.UnsupportedInterfaceError(
-            f"Unsupported serialization interface '{interface}'"
-        )
-
-    interface_module.initialize(celery, **interface_args)
