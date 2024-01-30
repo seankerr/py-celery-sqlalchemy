67bd1399ac93f603a5ec3405aa30d933512b0842
Sean Kerr <sean@code-box.org>
2024-01-30 09:30:07 -0800

    Fix json tests

--- tests/json/test__init__.py
+++ tests/json/test__init__.py
@@ -3,12 +3,12 @@
 # --------------------------------------------------------------------------------------
 
 # celery-sqlalchemy types
+from celery_sqlalchemy.json import JsonSerializer
+
 from celery_sqlalchemy import errors
-from celery_sqlalchemy import json
 
 # system imports
 from typing import Any
-from typing import List
 
 from unittest.mock import Mock
 from unittest.mock import call
@@ -22,19 +22,80 @@ import orjson
 PATH = "celery_sqlalchemy.json"
 
 
-@patch(f"{PATH}.deserialize_arg")
-def test_arg_from_json__deserialize_arg(deserialize_arg: Mock) -> None:
-    arg = Mock()
+def test__init___set_json_key() -> None:
+    serializer = JsonSerializer(json_key="test")
 
-    assert json.arg_from_json(arg) == deserialize_arg.return_value
+    assert serializer.json_key == "test"
+
+
+def test__init___set_naive_utc_false() -> None:
+    serializer = JsonSerializer(naive_utc=False)
+
+    assert serializer.orjson_opts & orjson.OPT_NAIVE_UTC == 0
+
+
+def test__init___set_naive_utc_true() -> None:
+    serializer = JsonSerializer(naive_utc=True)
+
+    assert serializer.orjson_opts & orjson.OPT_NAIVE_UTC == orjson.OPT_NAIVE_UTC
+
+
+def test__init___set_passthrough_dataclass_false() -> None:
+    serializer = JsonSerializer(passthrough_dataclass=False)
+
+    assert serializer.orjson_opts & orjson.OPT_PASSTHROUGH_DATACLASS == 0
+
+
+def test__init___set_passthrough_dataclass_true() -> None:
+    serializer = JsonSerializer(passthrough_dataclass=True)
+
+    assert (
+        serializer.orjson_opts & orjson.OPT_PASSTHROUGH_DATACLASS
+        == orjson.OPT_PASSTHROUGH_DATACLASS
+    )
+
+
+def test__init___set_on_deserialize_arg() -> None:
+    deserialize_arg = Mock()
+    serializer = JsonSerializer(on_deserialize_arg=deserialize_arg)
+
+    assert serializer.deserialize_arg == deserialize_arg
+
+
+def test__init___set_on_serialize_arg() -> None:
+    serialize_arg = Mock()
+    serializer = JsonSerializer(on_serialize_arg=serialize_arg)
+
+    assert serializer.serialize_arg == serialize_arg
+
+
+def test__init___set_utc_z_false() -> None:
+    serializer = JsonSerializer(utc_z=False)
+
+    assert serializer.orjson_opts & orjson.OPT_UTC_Z == 0
+
+
+def test__init___set_utc_z_true() -> None:
+    serializer = JsonSerializer(utc_z=True)
+
+    assert serializer.orjson_opts & orjson.OPT_UTC_Z == orjson.OPT_UTC_Z
+
+
+def test_arg_from_json__deserialize_arg() -> None:
+    arg = Mock()
+    deserialize_arg = Mock()
+    serializer = JsonSerializer(on_deserialize_arg=deserialize_arg)
+
+    assert serializer.arg_from_json(arg) == deserialize_arg.return_value
 
     deserialize_arg.assert_called_with(arg)
 
 
 def test_arg_from_json__list() -> None:
     arg = [Mock()]
+    serializer = JsonSerializer()
 
-    result = json.arg_from_json(arg)
+    result = serializer.arg_from_json(arg)
 
     assert result == arg
     assert id(result) != id(arg)
@@ -62,8 +123,9 @@ def test_arg_from_json__model_class(
         "$model_path$": model_path,
         "name": name,
     }
+    serializer = JsonSerializer()
 
-    result = json.arg_from_json(arg)
+    result = serializer.arg_from_json(arg)
 
     schema_for_model_path.assert_called_with(model_path, sys.modules[__name__])
     field.from_json.assert_called_with(field, name)
@@ -92,8 +154,9 @@ def test_arg_from_json__model_instance(
         "$model_path$": model_path,
         "name": name,
     }
+    serializer = JsonSerializer()
 
-    result = json.arg_from_json(arg)
+    result = serializer.arg_from_json(arg)
 
     schema_for_model_path.assert_called_with(model_path, sys.modules[__name__])
     field.from_json.assert_called_with(field, name)
@@ -105,8 +168,9 @@ def test_arg_from_json__model_instance(
 
 def test_arg_from_json__other_than_model() -> None:
     arg = Mock()
+    serializer = JsonSerializer()
 
-    assert json.arg_from_json(arg) == arg
+    assert serializer.arg_from_json(arg) == arg
 
 
 @patch(f"{PATH}.schema_map_key")
@@ -128,8 +192,9 @@ def test_arg_to_json__model(
     schema = Mock(fields=[field])
     schema_for_model.return_value = schema
     arg = Mock(__table__=Mock())
+    serializer = JsonSerializer()
 
-    assert json.arg_to_json(arg) == {
+    assert serializer.arg_to_json(arg) == {
         "$model_path$": schema_map_key.return_value,
         field.name: field.to_json.return_value,
     }
@@ -140,17 +205,20 @@ def test_arg_to_json__model(
     schema_map_key.assert_called_with(arg)
 
 
-@patch(f"{PATH}.serialize_arg")
-def test_arg_to_json__serialize_arg(serialize_arg: Mock) -> None:
+def test_arg_to_json__serialize_arg() -> None:
     arg = type("FakeType")
+    serialize_arg = Mock()
+    serializer = JsonSerializer(on_serialize_arg=serialize_arg)
 
-    assert json.arg_to_json(arg) == serialize_arg.return_value
+    assert serializer.arg_to_json(arg) == serialize_arg.return_value
 
     serialize_arg.assert_called_with(arg)
 
 
 def test_arg_to_json__set() -> None:
-    assert json.arg_to_json({"test"}) == ["test"]
+    serializer = JsonSerializer()
+
+    assert serializer.arg_to_json({"test"}) == ["test"]
 
 
 def test_arg_to_json__unserializable_with_table__raises_serialization_error() -> None:
@@ -158,23 +226,23 @@ def test_arg_to_json__unserializable_with_table__raises_serialization_error() ->
         __table__ = "invalid"
 
     arg = Model()
+    serializer = JsonSerializer()
 
     with raises(errors.SerializationError) as ex:
-        assert json.arg_to_json(arg)
+        assert serializer.arg_to_json(arg)
 
     assert str(ex.value) == f"Cannot serialize type '{arg.__class__.__name__}'"
 
 
-@patch(f"{PATH}.serialize_arg")
-def test_arg_to_json__unserializable_with_table__serialize_arg(
-    serialize_arg: Mock,
-) -> None:
+def test_arg_to_json__unserializable_with_table__serialize_arg() -> None:
     class Model:
         __table__ = "invalid"
 
     arg = Model()
+    serialize_arg = Mock()
+    serializer = JsonSerializer(on_serialize_arg=serialize_arg)
 
-    assert json.arg_to_json(arg) == serialize_arg.return_value
+    assert serializer.arg_to_json(arg) == serialize_arg.return_value
 
     serialize_arg.assert_called_with(arg)
 
@@ -183,257 +251,60 @@ def test_arg_to_json__unserializable_without_table__raises_serialization_error()
     None
 ):
     arg = object()
+    serializer = JsonSerializer()
 
     with raises(errors.SerializationError) as ex:
-        assert json.arg_to_json(arg)
+        assert serializer.arg_to_json(arg)
 
     assert str(ex.value) == f"Cannot serialize type '{arg.__class__.__name__}'"
 
 
-@patch(f"{PATH}.serialization")
-@patch(f"{PATH}.message_from_args")
-@patch(f"{PATH}.message_to_args")
-def test_initialize(
-    message_to_args: Mock, message_from_args: Mock, serialization: Mock
-) -> None:
-    celery = Mock()
-
-    json.initialize(celery)
-
-    serialization.register.assert_called_with(
-        "json+sqlalchemy",
-        message_from_args,
-        message_to_args,
-        "json",
-    )
-
-    assert celery.conf.accept_content == ["json+sqlalchemy"]
-    assert celery.conf.result_accept_content == ["json+sqlalchemy"]
-    assert celery.conf.task_serializer == "json+sqlalchemy"
-
-    from celery_sqlalchemy.json import deserialize_arg
-    from celery_sqlalchemy.json import json_module_key
-    from celery_sqlalchemy.json import orjson_opts
-    from celery_sqlalchemy.json import serialize_arg
-
-    assert not deserialize_arg
-    assert json_module_key == "$model_path$"
-    assert orjson_opts & orjson.OPT_NAIVE_UTC == orjson.OPT_NAIVE_UTC
-    assert orjson_opts & orjson.OPT_UTC_Z == 0
-    assert not serialize_arg
-
-
-@patch(f"{PATH}.serialization")
-@patch(f"{PATH}.message_from_args")
-@patch(f"{PATH}.message_to_args")
-def test_initialize__set_apply_serializer(
-    message_to_args: Mock, message_from_args: Mock, serialization: Mock
-) -> None:
-    celery = Mock()
-
-    json.initialize(celery, apply_serializer=False)
-
-    assert isinstance(celery.conf.accept_content, Mock)
-    assert isinstance(celery.conf.result_accept_content, Mock)
-    assert isinstance(celery.conf.task_serializer, Mock)
-
-
-@patch(f"{PATH}.serialization")
-@patch(f"{PATH}.message_from_args")
-@patch(f"{PATH}.message_to_args")
-def test_initialize__set_content_type(
-    message_to_args: Mock, message_from_args: Mock, serialization: Mock
-) -> None:
-    celery = Mock()
-
-    json.initialize(celery, content_type="test")
-
-    serialization.register.assert_called_with(
-        "test",
-        message_from_args,
-        message_to_args,
-        "json",
-    )
-
-    assert celery.conf.accept_content == ["test"]
-    assert celery.conf.result_accept_content == ["test"]
-    assert celery.conf.task_serializer == "test"
-
-
-@patch(f"{PATH}.serialization")
-@patch(f"{PATH}.message_from_args")
-@patch(f"{PATH}.message_to_args")
-def test_initialize__set_json_key(
-    message_to_args: Mock, message_from_args: Mock, serialization: Mock
-) -> None:
-    celery = Mock()
-
-    json.initialize(celery, json_key="test")
-
-    from celery_sqlalchemy.json import json_module_key
-
-    assert json_module_key == "test"
-
-
-@patch(f"{PATH}.serialization")
-@patch(f"{PATH}.message_from_args")
-@patch(f"{PATH}.message_to_args")
-def test_initialize__set_naive_utc_false(
-    message_to_args: Mock, message_from_args: Mock, serialization: Mock
-) -> None:
-    celery = Mock()
-
-    json.orjson_opts = 0
-
-    json.initialize(celery, naive_utc=False)
-
-    assert json.orjson_opts & orjson.OPT_NAIVE_UTC == 0
-
-
-@patch(f"{PATH}.serialization")
-@patch(f"{PATH}.message_from_args")
-@patch(f"{PATH}.message_to_args")
-def test_initialize__set_naive_utc_true(
-    message_to_args: Mock, message_from_args: Mock, serialization: Mock
-) -> None:
-    celery = Mock()
-
-    json.orjson_opts = 0
-
-    json.initialize(celery, naive_utc=True)
-
-    assert json.orjson_opts & orjson.OPT_NAIVE_UTC == orjson.OPT_NAIVE_UTC
-
-
-@patch(f"{PATH}.serialization")
-@patch(f"{PATH}.message_from_args")
-@patch(f"{PATH}.message_to_args")
-def test_initialize__set_passthrough_dataclass_false(
-    message_to_args: Mock, message_from_args: Mock, serialization: Mock
-) -> None:
-    celery = Mock()
-
-    json.orjson_opts = 0
-
-    json.initialize(celery, passthrough_dataclass=False)
-
-    assert json.orjson_opts & orjson.OPT_PASSTHROUGH_DATACLASS == 0
-
-
-@patch(f"{PATH}.serialization")
-@patch(f"{PATH}.message_from_args")
-@patch(f"{PATH}.message_to_args")
-def test_initialize__set_passthrough_dataclass_true(
-    message_to_args: Mock, message_from_args: Mock, serialization: Mock
-) -> None:
-    celery = Mock()
-
-    json.orjson_opts = 0
-
-    json.initialize(celery, passthrough_dataclass=True)
-
-    assert (
-        json.orjson_opts & orjson.OPT_PASSTHROUGH_DATACLASS
-        == orjson.OPT_PASSTHROUGH_DATACLASS
-    )
-
-
-@patch(f"{PATH}.serialization")
-@patch(f"{PATH}.message_from_args")
-@patch(f"{PATH}.message_to_args")
-def test_initialize__set_on_deserialize_arg(
-    message_to_args: Mock, message_from_args: Mock, serialization: Mock
-) -> None:
-    celery = Mock()
-
-    json.initialize(celery, on_deserialize_arg=lambda x: x)
-
-    from celery_sqlalchemy.json import deserialize_arg
-
-    assert deserialize_arg
-
-
-@patch(f"{PATH}.serialization")
-@patch(f"{PATH}.message_from_args")
-@patch(f"{PATH}.message_to_args")
-def test_initialize__set_on_serialize_arg(
-    message_to_args: Mock, message_from_args: Mock, serialization: Mock
-) -> None:
-    celery = Mock()
-
-    json.initialize(celery, on_serialize_arg=lambda x: x)
-
-    from celery_sqlalchemy.json import serialize_arg
-
-    assert serialize_arg
-
-
-@patch(f"{PATH}.serialization")
-@patch(f"{PATH}.message_from_args")
-@patch(f"{PATH}.message_to_args")
-def test_initialize__set_utc_z_false(
-    message_to_args: Mock, message_from_args: Mock, serialization: Mock
-) -> None:
-    celery = Mock()
-
-    json.orjson_opts = 0
-
-    json.initialize(celery, utc_z=False)
-
-    assert json.orjson_opts & orjson.OPT_UTC_Z == 0
-
-
-@patch(f"{PATH}.serialization")
-@patch(f"{PATH}.message_from_args")
-@patch(f"{PATH}.message_to_args")
-def test_initialize__set_utc_z_true(
-    message_to_args: Mock, message_from_args: Mock, serialization: Mock
-) -> None:
-    celery = Mock()
-
-    json.orjson_opts = 0
-
-    json.initialize(celery, utc_z=True)
-
-    assert json.orjson_opts & orjson.OPT_UTC_Z == orjson.OPT_UTC_Z
-
-
 @patch(f"{PATH}.orjson")
-@patch(f"{PATH}.orjson_opts")
-def test_message_from_args(orjson_opts: Mock, orjson: Mock) -> None:
+def test_message_from_args(orjson: Mock) -> None:
     args = Mock()
+    serializer = JsonSerializer()
 
-    assert json.message_from_args(args) == orjson.dumps.return_value
+    assert serializer.message_from_args(args) == orjson.dumps.return_value
 
-    orjson.dumps.assert_called_with(args, default=json.arg_to_json, option=orjson_opts)
+    orjson.dumps.assert_called_with(
+        {
+            "arg": args.arg,
+            "args": args.args,
+            "kwargs": args.kwargs,
+        },
+        default=serializer.arg_to_json,
+        option=serializer.orjson_opts,
+    )
 
 
-@patch(f"{PATH}.arg_from_json")
+@patch(f"{PATH}.JsonSerializer.arg_from_json")
 @patch(f"{PATH}.orjson")
 def test_message_to_args(orjson: Mock, arg_from_json: Mock) -> None:
     message = Mock()
+    arg = Mock()
     arg_value = Mock()
     kwarg_value = Mock()
-    data: List[Any] = [
-        [arg_value],
-        {"name": kwarg_value},
-    ]
-    orjson.loads.return_value = data
+    json = {
+        "arg": arg,
+        "args": [arg_value],
+        "kwargs": {"name": kwarg_value},
+    }
+    orjson.loads.return_value = json
+    serializer = JsonSerializer()
 
-    assert json.message_to_args(message) == orjson.loads.return_value
+    args = serializer.message_to_args(message)
 
     orjson.loads.assert_called_with(message)
 
-    assert arg_from_json.call_args_list == [call(arg_value), call(kwarg_value)]
+    assert arg_from_json.call_args_list == [
+        call(arg),
+        call(arg_value),
+        call(kwarg_value),
+    ]
 
-    assert data[0][0] == arg_from_json()
-    assert data[1]["name"] == arg_from_json()
-
-
-@patch(f"{PATH}.enumerate")
-@patch(f"{PATH}.orjson")
-def test_message_to_args__celery_message(orjson: Mock, enumerate: Mock) -> None:
-    enumerate.side_effect = KeyError()
-    message = Mock()
-
-    assert json.message_to_args(message) == orjson.loads.return_value
+    assert args.arg
+    assert args.arg == arg_from_json()
+    assert args.args
+    assert args.args[0] == arg_from_json()
+    assert args.kwargs
+    assert args.kwargs["name"] == arg_from_json()
